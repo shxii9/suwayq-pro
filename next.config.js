ad56f24a89e1ae31af7fea98219a3fe88237309a
shxii9 <shxii9rt@gmail.com>
2025-12-29 19:12:26 +0300

    Final: Transform project into an Installable PWA

---
+++ next.config.js
@@ -0,0 +1,7 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com', 'cdn-icons-png.flaticon.com'],
+  },
+}
+module.exports = nextConfig
